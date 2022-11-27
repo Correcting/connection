6d549720f98f603521647f960115897701a55b48
Correcting <117009969+Correcting@users.noreply.github.com>
2022-11-27 04:42:07 +0100

    update brain.js

--- brain.js
+++ brain.js
@@ -162,7 +162,7 @@ function parsTup(str) {
 
 //
 function parsTdown(str) {
-  let parse = str.match(/(\d+) ч. (\d+) мин./s)
+  let parse = str.match(/(\d+) ч. (\d+) мин./)
 }
 
 // --------------------
