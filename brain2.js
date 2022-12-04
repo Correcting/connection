e2c5972314b12143ab8dfad06ce443fe2544cdcf
jamesfirstv <40821645+jamesfirstv@users.noreply.github.com>
2022-12-04 02:15:58 +0100

    update brain2.js

--- brain2.js
+++ brain2.js
@@ -38,7 +38,7 @@ module.exports = class {
     if (msg.n == 'peer') {
       anPeer(msg)
       console.log('Статусы:', global.st)
-      if (st.act) actPeer(msg)
+      if (global.st.act) actPeer(msg)
       console.log('Исходящие:', global.out)
     }
   /*
